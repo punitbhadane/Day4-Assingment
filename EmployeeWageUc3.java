7e9567e66953ed23a70e88eaf4f4fd8f1ed01b4d
punitbhadane <punitbhadane@gmail.com>
2022-09-28 18:34:01 +0530

    All program fiels

---
+++ EmployeeWageUc3.java
@@ -0,0 +1,32 @@
+package Day4;
+
+import java.util.Random;
+
+public class EmployeeWageUc3 {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int rand =random.nextInt(3);
+		
+		int wagePerHour=20;
+		int fulltime=12;
+		
+		//This is part time employee problem.
+		int partTime=8;
+		if(rand==2) {
+			System.out.println("You Worked Full Time Today");
+			int dailyWage;
+			dailyWage=fulltime*wagePerHour;
+			System.out.println("Your Full Time Salary is: "+ dailyWage +"Rs");
+		}
+		else if(rand == 1) {
+			System.out.println("You Worked Part Time Today");
+			int partTimeWage;
+			partTimeWage=partTime*wagePerHour;
+			System.out.println("Your Part Time Salary is: "+ partTimeWage +"Rs");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
+
+}
